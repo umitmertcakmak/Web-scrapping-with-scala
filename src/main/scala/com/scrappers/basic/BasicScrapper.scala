c1d5033cc2e0da7304d0f8b06c0beeaabd8cd8f9
Umit Mert Cakmak <umitmertcakmak@gmail.com>
2017-06-01 18:40:26 +0200

    get all the links

--- src/main/scala/com/scrappers/basic/BasicScrapper.scala
+++ src/main/scala/com/scrappers/basic/BasicScrapper.scala
@@ -8,22 +8,40 @@ import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+import java.io._
+
+import net.ruippeixotog.scalascraper.scraper.ContentExtractors.element
+
+import scala.util.Try
 
 object BasicScrapper extends App{
 
   val browser = JsoupBrowser()
   val doc = browser.get("https://www.bloomberg.com/europe")
 
+  val a_s = doc >> elementList("a")
 
-  val content = doc >> text(".home__markets-bar")
+  val writer = new PrintWriter(new File("elements.txt" ))
 
-  val latest = doc >> elementList(".markets-bar-item__link")
-  val topnews = doc >> elementList(".top-news-v3-story-thumbnail")
+  a_s.map(e => Try(writer.write(e.attr("href").toString + "\n")))
 
-  println("Latest News")
-  latest.map(e => println(e.attr("href")))
-  println("Top News")
-  topnews.map(e => println(e.attr("href")))
+  writer.close()
+
+//  val a_elements = doc >> elementList(".markets-bar-collection a")
+//
+//  val links = a_elements.map(_ >> attr("href"))
+//
+//  println(links)
+//
+//  val content = doc >> text(".home__markets-bar")
+//
+//  val latest = doc >> elementList(".markets-bar-item__link")
+//  val topnews = doc >> elementList(".top-news-v3-story-thumbnail")
+//
+//  println("Latest News")
+//  latest.map(e => println(e.attr("href")))
+//  println("Top News")
+//  topnews.map(e => println(e.attr("href")))
 //  val latestnews = browser.get(latest(0).attr("href"))
 //
 //  val newsbody = latestnews >> text(".body-copy")
